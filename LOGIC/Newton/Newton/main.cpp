30ed9b6dd79688d3a2515df694b59c64bc1adb3c
DragoonXVIII <99823997+DragoonXVIII@users.noreply.github.com>
2024-01-13 12:20:40 +0100

    Newton
    laptop->home

--- LOGIC/Newton/Newton/main.cpp
+++ LOGIC/Newton/Newton/main.cpp
@@ -4,6 +4,59 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+    int n;
+    cout<<"Wprowadz liczbe wezlow: ";
+    cin>>n;
+
+    double x[n];
+    double y[n];
+    double NewtonDQ[n]; //Differential quotients
+    double searchedX;
+
+    cout<<"Wprowadz wartosci wezlow: "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>x[n];
+    }
+
+    cout<<"Wprowadz wartosci funkcji w kolejnych wezlach: "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>y[n];
+    }
+
+    cout << "Podaj punkt, ktorego wartosci funkcji szukasz: " << endl;
+    cin >> searchedX;
+
+
+    //sprawdzanie
+    for(int c = 0; c < n; c++)
+    {
+        if(c != 0)
+        {
+            if(x[c] <= x[c - 1])
+            {
+                if (x[c] < x[c - 1])
+                    cout << "error: ciag nieposortowany" << endl;
+                else
+                    cout << "error: ciag posiada powtorzenia" << endl;
+                return 0;
+            }
+        }
+    }
+    if(searchedX >= x[0] && searchedX <= x[n - 1])
+    {
+        cout << "Dane wprowadzone prawidlowo." << endl;
+    }
+    else
+    {
+        cout << "error: szukany x jest poza zasiegiem wezlow interpolacyjnych" << endl;
+        return 0;
+    }
+
+
+
+
     return 0;
 }
