13ad5381cfe7f321be4b99fbe52823f5045e2618
DragoonXVIII <99823997+DragoonXVIII@users.noreply.github.com>
2024-01-29 01:35:25 +0100

    LOGIC - Newton_Method
    Swapped formulas

--- AAN-Z-APP/c++/logic/Newton_Method/Newton_Method/main.cpp
+++ AAN-Z-APP/c++/logic/Newton_Method/Newton_Method/main.cpp
@@ -5,30 +5,41 @@
 using namespace std;
 
 
-double f(double x)
+double fz(double x)
 {
     return exp(x)-5*x-3;
 }
 
-
-double df(double x)
+double dfz(double x)
 {
     return exp(x)-5;
 }
 
+
+double f(double x)
+{
+    return sin(x)-(x/2.0);
+}
+
+double df(double x)
+{
+    return cos(x)-(1.0/2.0);
+}
 
 void newton(double x0, double epsilon)
 {
     double x = x0;
     int iterations = 0;
 
-    while (fabs(f(x)) >= epsilon)
+    while(fabs(f(x)) >= epsilon)
     {
-        x = x - f(x) / df(x);
+        x = x - (f(x) / df(x));
         iterations++;
+        cout << "iteracja: " << iterations << "; x = " << x << ", f(x) = " << f(x) << endl;
     }
     cout << "Rozwiazanie znalezione po " << iterations << " iteracjach: x = " << x << ", f(x) = " << f(x) << endl;
 }
+
 
 int main()
 {
